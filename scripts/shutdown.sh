7d859c3804b9624d9d36261af762eae2577cda59
Nicolas 'eowfenth' Deçordi dos Reis <eowfenth.services@gmail.com>
2020-04-25 18:00:08 -0300

    feat: shutdowns can check for different host and env vars

--- scripts/shutdown.sh
+++ scripts/shutdown.sh
@@ -1,24 +1,44 @@
 #!/bin/bash
 
-SERVER_DIR=~/hercules/
+HOST=""
+SERVER_DIR=~hercules
 LOGIN_SERVER=login_server
 CHAR_SERVER=char_server
 MAP_SERVER=map_server
+DEFAULT_DB_CONTAINER=yggdrasil-db
 
 cd $SERVER_DIR
 
+# Server Guard to check wheter it is running;
 if [[ \
     -e .$LOGIN_SERVER.pid || \
     -e .$CHAR_SERVER.pid || \
     -e .$MAP_SERVER.pid 
-]]; then
-    echo "The server is running. Shutting it down";
+]];
+then
+    echo "Server is running. Shutting it down";
     ./athena-start stop
+ else
+    echo "Server is not running."
 fi
 
-if [ "$(docker ps -q -f name=$SERVER_DB_CONTAINER)" ]; then
-    echo "The database is running. Shutting it down..."
+if [ $HOST == "gcp" ]; then
+    METADATA_URL="http://metadata.google.internal/computeMetadata/v1/instance/attributes/"
+    METADATA_HEADER='-H "Metadata-Flavor: Google"'
+    METADATA_VALUE=$(curl -s "${METADATA_URL}SERVER_DB_CONTAINER ${METADATA_HEADER}")
+    SERVER_DB_CONTAINER=${METADATA_VALUE:-$DEFAULT_DB_CONTAINER}
+    if [ $SERVER_DB_CONTAINER == $DEFAULT_DB_CONTAINER ];
+    then
+        echo "You can associate a metadata key to dynamic change the content of this script. Use SERVER_DB_CONTAINER key."
+    fi
+fi
+
+if [ "$(docker ps -q -f name=$SERVER_DB_CONTAINER)" ];
+then
+    echo "Database is running. Shutting it down..."
     docker container stop $SERVER_DB_CONTAINER
+else
+    echo "Database is not running."
 fi
 
 exit 0;
